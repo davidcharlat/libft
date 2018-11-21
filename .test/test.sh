0b81d65912c47280e3d655cb188de1734ed02dfd
davidcharlat <david_charlat@yahoo.fr>
2018-11-21 17:19:51 +0100

    travis

--- .test/test.sh
+++ .test/test.sh
@@ -4,7 +4,7 @@ errorExit()
   exit 1;
 }
 
-
+cd ./.test
 for file in 'ft_memset' 'ft_bzero' 'ft_memcpy' 'ft_memccpy' 'ft_memmove' 'ft_memchr' 'ft_memcmp' 'ft_strlen' 'ft_strdup' 'ft_strcpy' 'ft_strncpy' 'ft_strcat' 'ft_strncat' 'ft_strlcat' 'ft_strchr' 'ft_strrchr' 'ft_strstr' 'ft_strnstr' 'ft_strcmp' 'ft_strncmp' 'ft_atoi' 'ft_isalpha' 'ft_isdigit' 'ft_isalnum' 'ft_isascii' 'ft_isprint' 'ft_toupper' 'ft_tolower' 'ft_memalloc' 'ft_memdel'
 do
 	gcc -o testa -Wall -Wextra -Werror $file.test.c ../src/$file.c
